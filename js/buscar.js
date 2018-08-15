f0aced3330e0d7115a316a827d2db0400a52b7b2
claumart <scmc@live.com>
2018-08-15 15:49:02 -0600

    modificados archivos de formularios, agregado codigo AngularJS en el archivo buscar.js, agregadas directivas de angularJS en el archivo buscar.html

--- js/buscar.js
+++ js/buscar.js
@@ -2,4 +2,35 @@ var app = angular.module("searchApp", []);
 app.controller("searchCtrl", function($scope) {
 	$scope.tipo_fecha1 = "fecha_especifica";
 	$scope.tipo_fecha2 = "fecha_especifica";
+	//$scope.procedencia_select ="none"
+	$scope.procendenciaList = [
+        {"idProcedencia" : "1", "nombreDependencia" : "Catastro"},
+        {"idProcedencia" : "2", "nombreDependencia" : "Tierras"},
+        {"idProcedencia" : "3", "nombreDependencia" : "Finanzas"},
+        {"idProcedencia" : "4", "nombreDependencia" : "Trans450"}];
+    $scope.empleadoReceptorList = [
+        {"numEmpleado" : "1", "nombreEmpleado" : "Mirian"},
+        {"numEmpleado" : "2", "nombreEmpleado" : "Marielos"},
+        {"numEmpleado" : "3", "nombreEmpleado" : "Elsa"},
+        {"numEmpleado" : "4", "nombreEmpleado" : "Karen Bonilla"}];
+	$scope.copyProcedenciaValue = ()=>{
+		if($scope.parametro_busqueda=="procedencia"){
+			$scope.buscar_input = $scope.procedencia_select.nombreDependencia;
+		}
+	};
+	$scope.copyEmpleadoReceptorValue = ()=>{
+		if($scope.parametro_busqueda=="empleadoReceptor"){
+			$scope.buscar_input = $scope.empleado_receptor_select.nombreEmpleado;
+		}
+
+	};
+	$scope.copyAbogadoAsignadoValue = ()=>{
+		
+	}
+	$scope.copyDependicaRemisionValue = ()=>{
+		
+	}
+	$scope.copyEstadoExpedienteValue = ()=>{
+		
+	}
 });
